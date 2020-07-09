fa7c8f7a775a4cc47e79b5fcfd4b6e56a282b99b
Hyeim Yang <yanghyeimm@gmail.com>
2020-07-09 17:19:09 +0900

    modify auto-nft/rule_generator.py: add function to make rule file

--- auto-nft/rule_generator.py
+++ auto-nft/rule_generator.py
@@ -13,7 +13,6 @@ def generateMatchRule(ruleTemplate, keys, matchFields, actionName):
   kdic = {}
   for i in range(len(keys)):
     kdic[keys[i][0]] = matchFields[i]
-  print kdic
 
   # ethernet
   eth_srcAddr, eth_srcAddr_mask = 'hdr.eth.srcAddr' in kdic and (int(kdic['hdr.eth.srcAddr']), 2 ** 48 - 1) or (0, 0)
@@ -47,7 +46,7 @@ def translateRules(inst_id, rules, sortedCFG, tdic, rdic):
   # parameter (vdp_id, inst_id, stage_id, match_chain_bitmap, header_chain_bitmap)
   ruleTemplate = rules['cfg'][0]
   pipe = sortedCFG[0][2] < 3 and 'Ingress' or 'Egress'
-  stage_id = sortedCFG[0][2] % 3 # virtual stage number of first node
+  stage_id = sortedCFG[0][2] + 1 # virtual stage number of first node
   match_chain_bitmap = 4 # header match
   header_chain_bitmap = 1
   translateRules = ruleTemplate % (pipe, inst_id, inst_id, stage_id, match_chain_bitmap, header_chain_bitmap)
@@ -55,7 +54,7 @@ def translateRules(inst_id, rules, sortedCFG, tdic, rdic):
   
   for node in sortedCFG: # sortedCFG: [table_info, num_of_rules, virtual_stage_number]
     pipe = node[2] < 3 and 'Ingress' or 'Egress'
-    virtualStageNum = node[2] % 3
+    virtualStageNum = node[2] + 1
     if node[0][0] == 'apply': # apply table, match and action
       actions = tdic[node[0][1]]['action']
       if node[0][1] not in rdic: continue
@@ -82,167 +81,23 @@ def translateRules(inst_id, rules, sortedCFG, tdic, rdic):
           translatedRule = translatedRule + '(' + params + ')'
           if translatedRule not in rlist:
             rlist.append(translatedRule)
-    else: # condition statement (if/else), just match
-      print "if: ", node[0][1]
+  return rlist
+
+def makeFiles(rlist, target):
+  f = open('tmp.py', 'w')
+
+  head = open('switchInfo/' + target + '/rule_form/head', 'r')
+  f.write(head.read())
+  head.close()
 
   print "\n\n -- translated rule list -- "
   for v in rlist:
-    print v
-  return rlist
+    f.write("    " + v + "\n")
 
-def makeFiles():
-  # import rule template
-  # make new p4 ruleset
-  # make P4 ruleset file
-  global pcnt
+  tail = open('switchInfo/' + target + '/rule_form/tail', 'r')
+  f.write(tail.read())
+  tail.close()
 
-  f1 = open('switchInfo/' + self.name + '/rule_form/' + vals[0], 'r')
-  f2 = open('tmp', 'w')
+  f.close()
 
-  forms = []
-  for line in f1:
-    forms.append(line)  
-    insnum = -1
-
-  output = ''
-  cfg = ''; dstMAC = ''; fwdport = ''; dstIP = ''
-  tgtIP = ''; srcIP = ''; dstport = ''; opcode = ''
-  # l2fwd [dstMAC] [fwdport]
-  if vals[0] == 'l2fwd':
-    insnum = 0
-    if reqop == 'insert':
-      if 'cfg' not in l2fwd:
-        cfg += forms[1]  
-        cfg += forms[2]  
-        l2fwd['cfg'] = [cfg, pcnt]
-        resourceStatus['L2 Forwarding'][pcnt] += 2 
-      if 'dstMAC' + vals[1] not in l2fwd:
-        macnum = vals[1].split(':')
-        rule = forms[4] % (seqnum[insnum], int(macnum[0], 16), int(macnum[1], 16), int(macnum[2], 16), int(macnum[3], 16), int(macnum[4], 16), int(macnum[5], 16))
-        dstMAC += rule
-        rule = forms[0] % (int(macnum[0], 16), int(macnum[1], 16), int(macnum[2], 16), int(macnum[3], 16), int(macnum[4], 16), int(macnum[5], 16), seqnum[insnum])
-        dstMAC += rule
-        l2fwd['dstMAC' + vals[1]] = [dstMAC, pcnt]
-        resourceStatus['L2 Forwarding'][pcnt] += 2 
-      if 'fwdport' + vals[2] not in l2fwd:
-        rule = forms[3] % (seqnum[insnum], int(vals[2]))
-        fwdport += rule
-        l2fwd['fwdport' + vals[2]] = [fwdport, pcnt]
-        resourceStatus['L2 Forwarding'][pcnt] += 1 
-    else: # reqop == 'delete'
-      if 'dstMAC' + vals[1] in l2fwd:
-        resourceStatus['L2 Forwarding'][l2fwd['dstMAC' + vals[1]][1]] -= 2 
-        del l2fwd['dstMAC' + vals[1]]
-      if 'fwdport' + vals[2] in l2fwd:
-        resourceStatus['L2 Forwarding'][l2fwd['fwdport' + vals[2]][1]] -= 1 
-        del l2fwd['fwdport' + vals[2]]
-      if len(l2fwd) == 1: # if all of the rules deleted
-        resourceStatus['L2 Forwarding'][l2fwd['cfg'][1]] -= 2 
-        del l2fwd['cfg']
-
-  # l3fwd [dstIP] [fwdport]
-  elif vals[0] == 'l3fwd':
-    insnum = 1
-    if 'cfg' not in l3fwd:
-      cfg += forms[0]  
-      cfg += forms[1]  
-      cfg += forms[2]  
-      l3fwd['cfg'] = cfg
-      resourceStatus['L3 Forwarding'][pcnt] += 3
-    if 'dstIP' + vals[1] not in l3fwd: 
-      ipnum = vals[1].split('.')
-      rule = forms[3] % (int(ipnum[0]), int(ipnum[1]), int(ipnum[2]), int(ipnum[3]), seqnum[insnum])
-      dstIP += rule
-      l3fwd['dstIP' + vals[1]] = dstIP
-      resourceStatus['L3 Forwarding'][pcnt] += 1
-    if 'fwdport' + vals[2] not in l3fwd:
-      rule = forms[4] % (seqnum[insnum], int(vals[2]))
-      fwdport += rule
-      l3fwd['fwdport' + vals[2]] = fwdport
-      resourceStatus['L3 Forwarding'][pcnt] += 1
-
-  # fw [op] [srcIP] [dstport] [fwdport]
-  elif vals[0] == 'fw':
-    insnum = 2
-    if 'cfg' not in fw:
-      cfg += forms[0]  
-      cfg += forms[1]  
-      fw['cfg'] = cfg
-      resourceStatus['Firewall'][pcnt] += 2
-    if vals[1] == 'drop':
-      if vals[1] + 'cfg' not in fw:
-        cfg += forms[2]
-        fw[vals[1] + 'cfg'] = forms[2]
-        resourceStatus['Firewall'][pcnt] += 1
-      if vals[2] != 'N' and 'srcIP' + vals[2] not in fw:
-        ipnum = vals[2].split('.')
-        rule = forms[4] % (int(ipnum[0]), int(ipnum[1]), int(ipnum[2]), int(ipnum[3]), seqnum[insnum])
-        srcIP += rule
-        fw['srcIP' + vals[2]] = srcIP
-        resourceStatus['Firewall'][pcnt] += 1
-      if vals[3] != 'N' and 'dstport' + vals[3] not in fw:
-        rule = forms[6] % (int(vals[3]), seqnum[insnum])
-        dstport += rule
-        fw['dstport' + vals[3]] = dstport
-        resourceStatus['Firewall'][pcnt] += 1
-
-    elif vals[1] == 'fwd':
-      if vals[2] != 'N' and 'srcIP' + vals[2] not in fw:
-        ipnum = vals[2].split('.')
-        rule = forms[3] % (int(ipnum[0]), int(ipnum[1]), int(ipnum[2]), int(ipnum[3]), seqnum[insnum])
-        srcIP += rule
-        fw['srcIP' + vals[2]] = srcIP
-        resourceStatus['Firewall'][pcnt] += 1
-      if vals[3] != 'N' and 'dstport' + vals[3] not in fw:
-        rule = forms[5] % (int(vals[3]), seqnum[insnum])
-        dstport += rule
-        fw['dstport' + vals[3]] = dstport
-        resourceStatus['Firewall'][pcnt] += 1
-      if vals[4] != 'N' and 'fwdport' + vals[4] not in fw:
-        rule = forms[7] % (seqnum[insnum], int(vals[4]))
-        fwdport += rule
-        fw['fwdport' + vals[4]] = fwdport
-        resourceStatus['Firewall'][pcnt] += 1
-
-  # arp [opcode] [tgtIP] [dstMAC]
-  elif vals[0] == 'arp':
-    insnum = 3
-    if 'cfg' not in arp:
-      cfg += forms[1]
-      cfg += forms[2]  
-      cfg += forms[3]  
-      cfg += forms[4]  
-      cfg += forms[6]  
-      cfg += forms[8]  
-      arp['cfg'] = cfg
-      resourceStatus['ARP Proxy'][pcnt] += 6
-    if 'op' + vals[1] not in arp:
-      rule = forms[0] % (int(vals[1]), seqnum[insnum])
-      opcode += rule
-      arp['op' + vals[1]] = opcode
-      resourceStatus['ARP Proxy'][pcnt] += 1
-    if 'ip' + vals[2] + 'mac' + vals[3] not in arp:
-      # tgtIP
-      ipnum = vals[2].split('.')
-      rule = forms[7] % (int(ipnum[0]), int(ipnum[1]), int(ipnum[2]), int(ipnum[3]))
-      tgtIP += rule
-      # dstMAC
-      macnum = vals[3].split(':')
-      rule = forms[5] % (int(macnum[0], 16), int(macnum[1], 16), int(macnum[2], 16), int(macnum[3], 16), int(macnum[4], 16), int(macnum[5], 16))
-      dstMAC += rule
-      arp['ip' + vals[2] + 'mac' + vals[3]] = tgtIP + dstMAC
-      resourceStatus['ARP Proxy'][pcnt] += 2
-
-  ruleset = cfg + opcode + dstMAC + srcIP + tgtIP + dstIP + fwdport + dstport
-  f2.write(ruleset)
-
-  if reqop == 'insert' and ruleset != '': 
-    seqnum[insnum] += 1
-    pcnt = (pcnt + 1) % 4
-
-  f2.close()
-  f1.close()
   return
-
-# rules = getRuleTemplate("tofino")
-# print rules
